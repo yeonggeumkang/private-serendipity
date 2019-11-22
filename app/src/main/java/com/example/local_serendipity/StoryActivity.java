0581c97d6a9293d0bf2059dca8cdfca3bd6c0e3e
yeonggeumkang <realyoungm@naver.com>
2019-11-22 15:10:56 +0900

    create story activity

---
+++ app/src/main/java/com/example/local_serendipity/StoryActivity.java
@@ -0,0 +1,16 @@
+package com.example.local_serendipity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StoryActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+    }
+}
