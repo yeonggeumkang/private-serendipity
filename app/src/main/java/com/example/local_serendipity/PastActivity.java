e1b1064234a7310c9373905122d69bbfc11d7fe5
yeonggeumkang <realyoungm@naver.com>
2019-11-29 13:11:07 +0900

    recycler view 수정중

--- app/src/main/java/com/example/local_serendipity/PastActivity.java
+++ app/src/main/java/com/example/local_serendipity/PastActivity.java
@@ -3,6 +3,10 @@ package com.example.local_serendipity;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class PastActivity extends AppCompatActivity {
 
@@ -11,6 +15,19 @@ public class PastActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_past);
+
+        ArrayList<String> list = new ArrayList<>();
+        for (int i=0; i<100; i++) {
+            list.add(String.format("사용자", i)) ;
+        }
+
+        // 리사이클러뷰에 LinearLayoutManager 객체 지정.
+        RecyclerView recyclerView = findViewById(R.id.recyclerView) ;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)) ;
+
+        // 리사이클러뷰에 SimpleTextAdapter 객체 지정.
+        RecyclerAdapter adapter = new RecyclerAdapter(list) ;
+        recyclerView.setAdapter(adapter) ;
 
 
 
