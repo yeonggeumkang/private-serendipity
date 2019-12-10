5adfd2a35f1f48feaa1da147a4648fea25366c65
yeonggeumkang <realyoungm@naver.com>
2019-12-10 11:41:41 +0900

    edit : 지역/키 spinner 생성

--- app/src/main/java/com/example/local_serendipity/EditProfileActivity.java
+++ app/src/main/java/com/example/local_serendipity/EditProfileActivity.java
@@ -1,6 +1,11 @@
 package com.example.local_serendipity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +15,24 @@ public class EditProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        Spinner spinnerCity = (Spinner)findViewById(R.id.spinner_city);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.city_array, R.layout.support_simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinnerCity.setAdapter(adapter);
+
+        spinnerCity.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
     }
 }
