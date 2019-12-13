682f14d105016ee197af536ab53920e8263f44b0
yeonggeumkang <realyoungm@naver.com>
2019-12-13 16:43:01 +0900

    specific interest 완성

--- app/src/main/java/com/example/local_serendipity/GridAdapter.java
+++ app/src/main/java/com/example/local_serendipity/GridAdapter.java
@@ -27,7 +27,7 @@ class GridAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return list.size();
+        return 1;
     }
 
     @Override
@@ -43,9 +43,42 @@ class GridAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         if (convertView == null)
-            convertView = inf.inflate(R.layout.layout_interest_item, null);
-        TextView tv = (TextView) convertView.findViewById(R.id.tv_item);
-        tv.setText((String) list.get(position));
+            convertView = inf.inflate(R.layout.layout_interest_items, null);
+        TextView tv1 = (TextView) convertView.findViewById(R.id.tv_item1);
+        TextView tv2 = (TextView) convertView.findViewById(R.id.tv_item2);
+        TextView tv3 = (TextView) convertView.findViewById(R.id.tv_item3);
+        TextView tv4 = (TextView) convertView.findViewById(R.id.tv_item4);
+        TextView tv5 = (TextView) convertView.findViewById(R.id.tv_item5);
+        TextView tv6 = (TextView) convertView.findViewById(R.id.tv_item6);
+        TextView tv7 = (TextView) convertView.findViewById(R.id.tv_item7);
+        TextView tv8 = (TextView) convertView.findViewById(R.id.tv_item8);
+        TextView tv9 = (TextView) convertView.findViewById(R.id.tv_item9);
+        TextView tv10 = (TextView) convertView.findViewById(R.id.tv_item10);
+        TextView tv11 = (TextView) convertView.findViewById(R.id.tv_item11);
+        TextView tv12 = (TextView) convertView.findViewById(R.id.tv_item12);
+        TextView tv13 = (TextView) convertView.findViewById(R.id.tv_item13);
+        TextView tv14 = (TextView) convertView.findViewById(R.id.tv_item14);
+        TextView tv15 = (TextView) convertView.findViewById(R.id.tv_item15);
+        TextView tv16 = (TextView) convertView.findViewById(R.id.tv_item16);
+        TextView tv17 = (TextView) convertView.findViewById(R.id.tv_item17);
+
+        tv1.setText((String) list.get(0));
+        tv2.setText((String) list.get(1));
+        tv3.setText((String) list.get(2));
+        tv4.setText((String) list.get(3));
+        tv5.setText((String) list.get(4));
+        tv6.setText((String) list.get(5));
+        tv7.setText((String) list.get(6));
+        tv8.setText((String) list.get(7));
+        tv9.setText((String) list.get(8));
+        tv10.setText((String) list.get(9));
+        tv11.setText((String) list.get(10));
+        tv12.setText((String) list.get(11));
+        tv13.setText((String) list.get(12));
+        tv14.setText((String) list.get(13));
+        tv15.setText((String) list.get(14));
+        tv16.setText((String) list.get(15));
+        tv17.setText((String) list.get(16));
 
         return convertView;
     }
--- app/src/main/java/com/example/local_serendipity/InterestSearchSpecActivity.java
+++ app/src/main/java/com/example/local_serendipity/InterestSearchSpecActivity.java
@@ -46,30 +46,34 @@ public class InterestSearchSpecActivity extends AppCompatActivity {
         //gridview
         ArrayList<String> list = new ArrayList<>();
         list.add("자전거");
-        list.add("요가/필라테스");
-        list.add("레슬링");
+        list.add("배드민턴");
         list.add("테니스/스쿼시");
         list.add("볼링");
-        list.add("스키");
+        list.add("스키/보드");
+        list.add("골프");
+
+        list.add("클라이밍");
+        list.add("다이어트");
         list.add("헬스/크로스핏");
         list.add("탁구");
+        list.add("요가/필라테스");
+        list.add("농구");
+
+        list.add("야구");
+        list.add("배구");
         list.add("스케이트/인라인");
+        list.add("펜싱");
         list.add("크루즈보드");
 
+            // 커스텀 아답타 생성
+        GridAdapter adapterA = new GridAdapter (getApplicationContext(),list);
+        GridView gv1 = (GridView)findViewById(R.id.gv_interest1);
+        GridView gv2 = (GridView)findViewById(R.id.gv_interest2);
+        GridView gv3 = (GridView)findViewById(R.id.gv_interest3);
 
-        // 커스텀 아답타 생성
-        GridAdapter adapter = new GridAdapter (getApplicationContext(),list);
-
-        GridView gv = (GridView)findViewById(R.id.gv_interest1);
-        gv.setAdapter(adapter);  // 커스텀 아답타를 GridView 에 적용
-
-        gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                //tv.setText("position : " + position);
-            }
-        });
+        gv1.setAdapter(adapterA);
+        gv2.setAdapter(adapterA);
+        gv3.setAdapter(adapterA);
 
     }
 
